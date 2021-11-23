4fdb2975d5ab423d326c82f9e3886523100e9454
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-11-23 15:43:09 -0300

    Entidade endereco

---
+++ src/main/java/com/project/springmvc/boot/domain/Endereco.java
@@ -0,0 +1,34 @@
+package com.project.springmvc.boot.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ENDERECOS")
+@Getter
+@Setter
+public class Endereco extends AbstractEntity<Long> {
+
+    @Column(nullable = false)
+    private String logradouro;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = false)
+    private String cidade;
+
+    @Column(nullable = false, length = 2)
+    @Enumerated(EnumType.STRING)
+    private UF uf;
+
+    @Column(nullable = false, length = 9)
+    private String cep;
+
+    @Column(nullable = false, length = 5)
+    private Integer numero;
+    private String complemento;
+
+}
